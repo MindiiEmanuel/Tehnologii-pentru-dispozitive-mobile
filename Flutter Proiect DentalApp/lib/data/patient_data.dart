749b82c6dfab78bc61128d488a21a51210eaf385
MindiiEmanuel <93757962+MindiiEmanuel@users.noreply.github.com>
2024-03-02 15:32:03 +0200

    Add dent_data boys, girls, patient and history

---
+++ Flutter Proiect DentalApp/lib/data/patient_data.dart
@@ -0,0 +1,64 @@
+class PatientData {
+  static List<Map<String, dynamic>> patients = [
+    {
+      'name': 'Popescu',
+      'forename': 'Ana',
+      'age': 8.3,
+      'gender': 'F',
+    },
+    {
+      'name': 'Ionescu',
+      'forename': 'Andrei',
+      'age': 12.7,
+      'gender': 'M',
+    },
+    {
+      'name': 'Stan',
+      'forename': 'Maria',
+      'age': 6.9,
+      'gender': 'F',
+    },
+    {
+      'name': 'Vasilescu',
+      'forename': 'Ion',
+      'age': 5.4,
+      'gender': 'M',
+    },
+    {
+      'name': 'Dragomir',
+      'forename': 'Elena',
+      'age': 10.1,
+      'gender': 'F',
+    },
+    {
+      'name': 'Pop',
+      'forename': 'Alex',
+      'age': 9.6,
+      'gender': 'M',
+    },
+    {
+      'name': 'Tudor',
+      'forename': 'Ana-Maria',
+      'age': 7.8,
+      'gender': 'F',
+    },
+    {
+      'name': 'Marinescu',
+      'forename': 'Andrei',
+      'age': 14.2,
+      'gender': 'M',
+    },
+    {
+      'name': 'Dumitrescu',
+      'forename': 'Andreea',
+      'age': 11.5,
+      'gender': 'F',
+    },
+    {
+      'name': 'Stoica',
+      'forename': 'Vlad',
+      'age': 6.7,
+      'gender': 'M',
+    },
+  ];
+}
---
+++ Flutter Proiect DentalApp/lib/data/patient_history_data.dart
@@ -0,0 +1,29 @@
+class PatientHistory {
+  String name;
+  String forename;
+  int age;
+  String gender;
+  String selectedIncisor1;
+  String selectedIncisor2;
+  String selectedCanine;
+  String selectedPremolar1;
+  String selectedPremolar2;
+  String selectedMolar1;
+  String selectedMolar2;
+  double ageOfTeeth;
+
+  PatientHistory({
+    required this.name,
+    required this.forename,
+    required this.age,
+    required this.gender,
+    required this.selectedIncisor1,
+    required this.selectedIncisor2,
+    required this.selectedCanine,
+    required this.selectedPremolar1,
+    required this.selectedPremolar2,
+    required this.selectedMolar1,
+    required this.selectedMolar2,
+    required this.ageOfTeeth,
+  });
+}
